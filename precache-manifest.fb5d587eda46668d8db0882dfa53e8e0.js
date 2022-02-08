749454af72f9e50089259783c91eff0fe45c8cd4
Tainaz Bano <98350927+tainaz-bano@users.noreply.github.com>
2022-02-08 22:34:41 +0530

    Updates

---
+++ precache-manifest.fb5d587eda46668d8db0882dfa53e8e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf242df98d2ff4ca50a441d2716d60ba",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "069c0f0acb49d1a41936",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "44770abcd4a12a3e0075",
+    "url": "/my-app/static/js/2.74e666b0.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/my-app/static/js/2.74e666b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "122ab136bf91f2401f1e",
+    "url": "/my-app/static/js/3.7b35ff27.chunk.js"
+  },
+  {
+    "revision": "069c0f0acb49d1a41936",
+    "url": "/my-app/static/js/main.90136c93.chunk.js"
+  },
+  {
+    "revision": "5d88640bb3f6f8ac46e7",
+    "url": "/my-app/static/js/runtime-main.ed509273.js"
+  }
+]);
